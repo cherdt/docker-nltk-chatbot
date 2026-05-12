af61dae649a2a2c183da36abb8a04c76a9c446df
Chris Herdt <cherdt@umn.edu>
2026-05-11 19:46:18 -0500

    Validate user and hash
    Fix #10

--- chatbot.py
+++ chatbot.py
@@ -4,6 +4,7 @@ from flask import request
 from flask_cors import CORS
 import nltk.chat
 from nltk.chat.util import Chat, reflections
+import re
 import requests
 import yaml
 app = Flask(__name__)
@@ -11,11 +12,16 @@ CORS(app)
 
 bots = {}
 
-def get_bot_from_gist(user, hash):
-    r = requests.get('https://gist.githubusercontent.com/' + user + '/' + hash + '/raw/bot.yaml')
-    if r.status_code != 200:
-        return "Bad request", r.status_code
-    chatbot = yaml.safe_load(r.text)
+# valid usernames may contain alphanumeric characters and hypens
+def is_valid_user(user):
+    return re.fullmatch("[-a-zA-Z0-9]+", user)
+
+# valid hashes are 32-digit hexidecimal numbers
+def is_valid_hash(hash):
+    return re.fullmatch("[a-f0-9]{32}", hash)
+
+def build_bot_from_yaml(yamltext, user, hash):
+    chatbot = yamltext
     bot = {}
     pairs = []
     for pair in chatbot:
@@ -28,6 +34,19 @@ def get_bot_from_gist(user, hash):
     bot["pairs"] = Chat(pairs, reflections)
     bots[hash] = {"name": bot["name"], "pairs": bot["pairs"]}
 
+
+def get_bot_from_gist(user, hash):
+    if is_valid_user(user) and is_valid_hash(hash):
+        r = requests.get('https://gist.githubusercontent.com/' + user + '/' + hash + '/raw/bot.yaml')
+        if r.status_code != 200:
+            return False
+        yamltext = yaml.safe_load(r.text)
+        build_bot_from_yaml(yamltext, user, hash)
+        return True
+    else:
+        return False
+
+
 @app.route("/chat-api")
 def what():
     text = request.args.get('text')
@@ -38,18 +57,27 @@ def what():
 
 @app.route("/chat/<user>/<hash>")
 def get_bot(user, hash):
-    if hash not in bots or request.args.get('reload'):
-       get_bot_from_gist(user, hash)
-    return render_template('chat.html', botname=bots[hash]["name"], user=user, intro=bots[hash]["pairs"].respond("intro"), hash=hash)
+    if hash in bots or request.args.get('reload') or get_bot_from_gist(user, hash):
+        bot = bots[hash]
+        bot_name = bot["name"]
+        intro = bot["pairs"].respond("intro")
+    else:
+        bot_name = "ELIZA"
+        intro = "Hello, what would you like to discuss today?"
+        user = "ERROR"
+        hash = "ERROR"
+    return render_template('chat.html', botname=bot_name, user=user, intro=intro, hash=hash)
  
 @app.route("/chat-api/<user>/<hash>")
 def get_bot_response(user, hash):
-    if hash not in bots:
-        get_bot_from_gist(user, hash)
+    if hash in bots or get_bot_from_gist(user, hash):
+        bot = bots[hash]["pairs"]
+    else:
+        bot = nltk.chat.eliza.eliza_chatbot
     text = request.args.get('text')
     if (not text):
         text = ""
-    return bots[hash]["pairs"].respond(text)
+    return bot.respond(text)
 
 @app.route("/test/")
 def test():
